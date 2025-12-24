8c47201460d7e8e9c56dd2e3f9fa4b31c007c133
mahmedadawy2013 <75279964+mahmedadawy2013@users.noreply.github.com>
2025-12-24 19:07:11 +0200

    Fix macro definitions and usage in UVM testbench

--- tb_coloured__uvm.sv
+++ tb_coloured__uvm.sv
@@ -1,5 +1,5 @@
 // Include UVM macros
-include "uvm_macros.svh"
+`include "uvm_macros.svh"
 
 // Import UVM package
 import uvm_pkg::*;
@@ -34,42 +34,19 @@ int dsa = 10;
         BACKGROUND_WHITE    = 24
     } PrintColorEnum;
 
-define uvmc_info(root,msg,level,msg_color)\
+`define uvmc_info(root,msg,level,msg_color)\
     case (msg_color)\
-        GREEN               : str_msg_color = "0;32m";\
-        YELLOW              : str_msg_color = "0;33m";\
-        BLUE                : str_msg_color = "0;34m";\
-        MAGENTA             : str_msg_color = "0;35m";\
-        CYAN                : str_msg_color = "0;36m";\
-        WHITE               : str_msg_color = "0;37m";\
-        BOLD_GREEN          : str_msg_color = "1;32m";\
-        BOLD_YELLOW         : str_msg_color = "1;33m";\
-        BOLD_BLUE           : str_msg_color = "1;34m";\
-        BOLD_MAGENTA        : str_msg_color = "1;35m";\
-        BOLD_CYAN           : str_msg_color = "1;36m";\
-        BOLD_WHITE          : str_msg_color = "1;37m";\
-        UNDERLINE_GREEN     : str_msg_color = "4;32m";\
-        UNDERLINE_YELLOW    : str_msg_color = "4;33m";\
-        UNDERLINE_BLUE      : str_msg_color = "4;34m";\
-        UNDERLINE_MAGENTA   : str_msg_color = "4;35m";\
-        UNDERLINE_CYAN      : str_msg_color = "4;36m";\
-        UNDERLINE_WHITE     : str_msg_color = "4;37m";\
-        BACKGROUND_GREEN    : str_msg_color = "42m";\
-        BACKGROUND_YELLOW   : str_msg_color = "43m";\
-        BACKGROUND_BLUE     : str_msg_color = "44m";\
-        BACKGROUND_MAGENTA  : str_msg_color = "45m";\
-        BACKGROUND_CYAN     : str_msg_color = "46m";\
-        BACKGROUND_WHITE    : str_msg_color = "47m";\
-        default             : str_msg_color = "1;34m";\
+        GREEN   : str_msg_color = "0;32m";\
+        YELLOW  : str_msg_color = "0;33m";\
     endcase\
-    uvm_info(root, $sformatf("%c[%s: %s %c[0m", 27,str_msg_color, msg , 27), level);\
+    `uvm_info(root, $sformatf("%c[%s: %s %c[0m", 27,str_msg_color, msg , 27), level);\
 
-define uvmc_error(root,msg)\
+`define uvmc_error(root,msg)\
     str_msg_color = "1;31m";\
-    uvm_error(root, $sformatf("%c[%s: %s %c[0m", 27,str_msg_color, msg , 27));\
+    `uvm_error(root, $sformatf("%c[%s: %s %c[0m", 27,str_msg_color, msg , 27));\
 
 initial begin 
-    uvmc_info("UVM_HELLO", $sformatf("SBA7O : %0d",dsa), UVM_MEDIUM, GREEN);
+    `uvmc_info("UVM_HELLO", $sformatf("SBA7O : %0d",dsa), UVM_MEDIUM, GREEN);
     #500ns;
     uvm_root::get().set_report_max_quit_count(7);
     for(int i = 0; i<10; i++) begin 
